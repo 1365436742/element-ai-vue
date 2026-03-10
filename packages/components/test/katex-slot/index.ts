ba1faf7d2d49796e29100ad9c2aa27a4771a5779
1365436742 <51362520+1365436742@users.noreply.github.com>
2026-03-10 11:17:57 +0800

    Create index.ts

---
+++ packages/components/test/katex-slot/index.ts
@@ -0,0 +1,85 @@
+import { Node, mergeAttributes } from '@tiptap/core'
+import { VueNodeViewRenderer } from '@tiptap/vue-3'
+import KatexNodeView from './index.vue'
+
+/**
+ * 块级 KaTeX 公式扩展
+ * 匹配 rehype-katex 生成的 <span class="katex-display"> 元素
+ * 优先级高于行内，确保 katex-display 先匹配，其子节点不再被行内规则处理
+ */
+export const KatexBlock = Node.create({
+  name: 'katexBlock',
+  group: 'block',
+  atom: true,
+
+  addAttributes() {
+    return {
+      rawHtml: {
+        default: '',
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'span.katex-display',
+        priority: 60,
+        getAttrs(dom) {
+          return {
+            rawHtml: (dom as HTMLElement).outerHTML,
+          }
+        },
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['div', mergeAttributes(HTMLAttributes, { 'data-type': 'katex-block' })]
+  },
+
+  addNodeView() {
+    return VueNodeViewRenderer(KatexNodeView)
+  },
+})
+
+/**
+ * 行内 KaTeX 公式扩展
+ * 匹配 rehype-katex 生成的 <span class="katex"> 元素
+ */
+export const KatexInline = Node.create({
+  name: 'katexInline',
+  group: 'inline',
+  inline: true,
+  atom: true,
+
+  addAttributes() {
+    return {
+      rawHtml: {
+        default: '',
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'span.katex',
+        priority: 51,
+        getAttrs(dom) {
+          return {
+            rawHtml: (dom as HTMLElement).outerHTML,
+          }
+        },
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['span', mergeAttributes(HTMLAttributes, { 'data-type': 'katex-inline' })]
+  },
+
+  addNodeView() {
+    return VueNodeViewRenderer(KatexNodeView)
+  },
+})
