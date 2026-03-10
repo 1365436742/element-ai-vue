6dd16aeb6f90d3a56aa92dafcad68e9e07c07ed0
1365436742 <51362520+1365436742@users.noreply.github.com>
2026-03-10 11:18:13 +0800

    Add Katex slot component for rendering math expressions

---
+++ packages/components/test/katex-slot/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <node-view-wrapper
+    :as="node.type.name === 'katexInline' ? 'span' : 'div'"
+    v-html="node.attrs.rawHtml"
+  />
+</template>
+
+<script setup lang="ts">
+import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-3'
+
+defineProps(nodeViewProps)
+</script>
