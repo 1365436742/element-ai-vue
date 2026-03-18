e3772ce79778b12bd61d50cb8e2e406e10238620
1365436742 <51362520+1365436742@users.noreply.github.com>
2026-03-18 15:01:13 +0800

    Refactor conversation item component structure

--- packages/components/conversation-item/index.vue
+++ packages/components/conversation-item/index.vue
@@ -1,22 +1,59 @@
 <template>
-  <div :class="ns.b()"></div>
+  <section :class="ns.b()">
+    <ul :class="ns.e('group')" v-for="(items, groupName) in groups">
+      <li
+        v-if="groupName !== 'undefined' && groupName !== 'null' && groupName"
+        :class="ns.e('title')"
+      >
+        <slot name="group" :groupName="groupName">
+          {{ groupName }}
+        </slot>
+      </li>
+      <li
+        v-for="item in items"
+        :class="[
+          ns.e('item'),
+          ns.is('active', item[keyName] === props.activeKey),
+        ]"
+        :key="item.key"
+        @click="handleClick(item)"
+      >
+        <slot name="item" :item="item">
+          {{ item.label }}
+        </slot>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup lang="ts">
 import { useNamespace, useTheme } from '@element-ai-vue/hooks'
-import { conversationItemProps } from './props'
 import { computed } from 'vue'
+import { groupBy } from 'lodash-es'
+import {
+  ConversationItem,
+  conversationItemProps,
+  CoversationItemEmitsType,
+} from './props'
 
 defineOptions({
   name: 'ElAConversationItem',
 })
 
+const emits = defineEmits<CoversationItemEmitsType>()
 const props = defineProps({
   ...conversationItemProps,
 })
 
+const groups = computed(() => groupBy(props.items, (item) => item.group))
 const ns = useNamespace('conversation-item')
 
 const themeRef = computed(() => props.theme)
 const { theme } = useTheme(themeRef)
+
+const handleClick = (item: ConversationItem) => {
+  const activeKey = item[props.keyName]
+  emits('update:activeKey', activeKey)
+  emits('activeChange', activeKey, item)
+}
 </script>
