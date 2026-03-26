776e92212768f1f77fbae2e38fd264fefc806cfb
1365436742 <51362520+1365436742@users.noreply.github.com>
2026-03-26 18:44:06 +0800

    Update index.vue

--- packages/components/conversation-popover/index.vue
+++ packages/components/conversation-popover/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div :class="ns.b()"></div>
+  <Teleport to="body" :disabled="!isMobileWidth">
+    <div :class="[ns.b(), ns.is('mobile', isMobileWidth && !closeMobile)]">
+      <div :class="ns.e('wrapper')">
+        <Transition
+          :name="ns.e('collapse')"
+          @before-leave="leaveing = true"
+          @after-leave="leaveing = false"
+        >
+          <div v-if="!props.collapse" :class="ns.e('content')">
+            <slot></slot>
+          </div>
+        </Transition>
+      </div>
+      <Transition :name="ns.e('mark')">
+        <div
+          v-if="isMobileWidth && !collapse"
+          :class="ns.e('mark')"
+          @click="emits('update:collapse', true)"
+        ></div>
+      </Transition>
+    </div>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
-import { useNamespace, useTheme } from '@element-ai-vue/hooks'
-import { conversationPopoverProps } from './props'
-import { computed } from 'vue'
+import { useDevice, useNamespace, useTheme } from '@element-ai-vue/hooks'
+import { conversationPopoverProps, ConversatioPopoverEmitsType } from './props'
+import { computed, ref } from 'vue'
 
 defineOptions({
   name: 'ElAConversationPopover',
@@ -14,8 +35,19 @@ defineOptions({
 const props = defineProps({
   ...conversationPopoverProps,
 })
+const emits = defineEmits<ConversatioPopoverEmitsType>()
 
 const ns = useNamespace('conversation-popover')
+const { isMobileWidth } = useDevice()
+
+const leaveing = ref(false)
+
+const closeMobile = computed(() => {
+  if (props.collapse) {
+    return !leaveing.value
+  }
+  return props.collapse
+})
 
 const themeRef = computed(() => props.theme)
 const { theme } = useTheme(themeRef)
